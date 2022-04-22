76b19be3cfeefee2198e98682e5d80fa5a7a758c
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2022-04-22 16:06:35 +0200

    share everyone who is there with everyone else

--- src/lobby.rs
+++ src/lobby.rs
@@ -34,16 +34,24 @@ impl Handler<Connect> for Lobby {
     type Result = ();
 
     fn handle(&mut self, msg: Connect, _: &mut Context<Self>) -> Self::Result {
+        // store the address
+        self.sessions.insert(msg.self_id, msg.addr);
+
         // send to everyone in the room that new uuid just joined
         self.sessions
             .keys()
             .filter(|conn_id| *conn_id.to_owned() != msg.self_id)
             .for_each(|conn_id| {
-                self.send_message(&format!("{} just joined!", msg.self_id), conn_id)
+                self.send_message(&format!("joined: {}", msg.self_id), conn_id)
             });
 
-        // store the address
-        self.sessions.insert(msg.self_id, msg.addr);
+        // send me all the uuids of everyone who is already there
+        self.sessions
+            .keys()
+            .filter(|conn_id| *conn_id.to_owned() != msg.self_id)
+            .for_each(|conn_id| {
+                self.send_message(&format!("present: {}", conn_id), &msg.self_id)
+            });
 
         // send self your new uuid
         self.send_message(&format!("your id is {}", msg.self_id), &msg.self_id);
